b271fe5bda1b9c04b0362e6c7fe32c3fcd155dc5
Jessy Vandoorn <jessy.vandoorn@student.howest.be>
2019-11-07 18:17:52 +0100

    login page setup

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,12 +2,15 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 import { Hello } from "./components/Hello";
+import { Login } from "./components/Login";
+
 import './App.css';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <Hello />
+      <Login />
     </div>
   );
 }
---
+++ my-app/src/components/Login.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+
+export interface LoginProps {}
+
+export const Login = (props: LoginProps) => <h1>Login page</h1>;
